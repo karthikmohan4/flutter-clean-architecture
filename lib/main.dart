ee812c9095da0281685aa585a3b7e1a439074a1e
karthikmohan4 <mkarthik556@gmail.com>
2026-01-04 23:59:29 +0530

    feat: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
 
+import 'package:enterprise/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runZonedGuarded(
     () {
       WidgetsFlutterBinding.ensureInitialized();
-      runApp(const MyApp());
+      runApp(const App());
     },
     (error, stackTrace) {
       log("run zoned guard error:$error");
@@ -16,36 +17,3 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Enterprise',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Container(),
-    );
-  }
-}
